8546db87d40b287a1f2c19d182de358207f419e8
Ofir Shalev <ofirshalev1@gmail.com>
2015-02-03 17:32:20 +0800

    added question to quiz 2

---
+++ quiz2.R
@@ -0,0 +1,86 @@
+# Solution by Ofir Shalev, February 2015
+----------------------------------------
+
+# Question 1
+# ----------
+# 
+# What is the delta method asymptotic standard error of sqrt(phat) where phat is X/n where 
+# X∼Binomial(n,p)?
+
+
+# Question 2
+# ----------
+# 
+# 
+# You are a bookie taking $1 bets on the NFL Ravens game. 
+# The odds you give betters determines their payout. 
+# If you give 4 to 1 odds against the Ravens winning, then if a person bets on the Ravens winning and they win, 
+# you owe them their original dollar, plus an additional $4 (a total of $5). 
+# If a person bets on the Ravens losing and they lose, then you owe them their original dollar back, 
+#     plus $0.25 (a total of $1.25).
+# 
+# Suppose you collect a > 0 one dollar bets on the Ravens winning and b > 0 one dollar bets on the Ravens losing. 
+# What should you set the odds so that, regardless of the outcome of the game, you neither win nor lose money? 
+# Note, in this case, the betters place their bets and learn the odds later. 
+# Note also, the odds are something that you set in this case, not a direct measure of probability or randomness.
+
+
+# Question 3
+# ----------
+# 
+# 
+
+# In a randomly sampled survey of self-reported stress levels from two occupations, the following data were obtained
+# High Stress     Low Stress
+# Professor 	    70
+# Lion Tamer 	15
+# 
+# What is the P-value for a Z score test of equality of the proportions of high stress?
+# (Note the notation 1e-5, for example, is 1×10−5).
+# 
+# 4e-15
+# 1e-14
+# 1e-5
+# 1e-3
+
+
+# Question 4
+# ----------
+# 
+# 
+# Consider the following data recording case status relative to an environmental exposure
+# Case 	Control
+# Exposed 	45 	21
+# Unexposed 	15 	52
+# 
+# What would be the estimated asymptotic standard error for the log relative risk for this data? 
+# Consider case status as the outcome. That is, we're interested in evaluating the ratio of the proportion of 
+# cases comparing exposed to unexposed groups.
+# 
+# Around 0.05
+# Around 1.25
+# Around 0.25
+# Around 1.05
+
+
+# Question 5
+# ----------
+# 
+# If x1∼Binomial(n1,p1) and x2∼Binomial(n2,p2) and independent Beta(2,2) priors are placed on p1 and p2, 
+# what is the posterior mean for p1−p2?
+
+
+
+# Question 6
+# ----------
+# 
+# Researchers conducted a blind taste test of Coke versus Pepsi. 
+# Each of four people was asked which of two blinded drinks given in random order that they preferred. 
+# The data was such that 3 of the 4 people chose Coke. 
+# Assuming that this sample is representative, report a P-value for a test of the hypothesis that 
+#     Coke is preferred to Pepsi using a **two sided** exact test.
+# 
+# Around 0.6
+# Around 0.5
+# Around 0.4
+# Around 0.3
